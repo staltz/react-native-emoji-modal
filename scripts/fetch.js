3537bd644f323471e525f25289257aa3d2eab1c1
Andre Staltz <andre@staltz.com>
2021-11-29 09:54:04 -0300

    simplify scripts/fetch.js

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,18 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const fetch = require('node-fetch');
 const pick = require('just-pick');
 const sortBy = require('just-sort-by');
-const pkg = require(path.join(__dirname, '../package.json'));
+const allEmojis = require('emoji-datasource/emoji.json');
 
-const VERSION = pkg.devDependencies['emoji-datasource'];
-const URL = `https://github.com/iamcal/emoji-data/raw/v${VERSION}/emoji.json`;
 const OUTPUT_FILE = path.join(__dirname, '../emoji.json');
 
 (async function main() {
-  const response = await fetch(URL);
-  const allEmojis = await response.json();
-
   const sortedEmojis = sortBy(allEmojis, 'sort_order');
 
   const liteAndSortedEmojis = sortedEmojis.map((emoji) =>

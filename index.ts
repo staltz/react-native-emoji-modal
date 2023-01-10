920fee3b306bf94258f59a885e3f81df459df512
Andre Staltz <andre@staltz.com>
2023-01-10 17:17:33 +0200

    fix import of dependency just-group-by

--- index.ts
+++ index.ts
@@ -12,8 +12,8 @@ import {
   TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import groupBy from 'just-group-by';
-import mapValues from 'just-map-values';
+const groupBy = require('just-group-by/index.js');
+const mapValues = require('just-map-values/index.js');
 const noop = () => {};
 
 interface Emoji {
